30956b1497eb4d7d184f5481ed6f8f18a98c2509
Leonardo Fonteles <fonteles.leonardo@gmail.com>
2024-01-06 11:21:58 -0300

    inicializei o projeto já com a tela inicial

---
+++ src/components/StartScreen.jsx
@@ -0,0 +1,13 @@
+import './StartScreen.css'
+
+const StartScreen = () => {
+  return (
+    <div className='start'>
+        <h1>Secret Word</h1>
+        <p>CLique no botão abaixo para começar</p>
+        <button>Jogar</button>
+    </div>
+  )
+}
+
+export default StartScreen;
