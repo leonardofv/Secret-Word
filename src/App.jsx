cbf7f52150403e4686775f69674d95ae27b8b2c6
Leonardo Fonteles <fonteles.leonardo@gmail.com>
2024-01-08 15:24:47 -0300

    criei os componentes Game e GameOver

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
+//CSS
 import './App.css'
-import StartScreen from './components/StartScreen'
+//React
+import { useCallback, useEffect, useState } from "react";
+//data
+import { wordsList } from './data/data';
+//components
+import StartScreen from './components/StartScreen';
+import Game from './components/Game';
+import GameOver from './components/GameOver';
+
+const stages = [ //estágios do jogo
+  {id: 1, name: "start"},
+  {id: 2, name: "game"},
+  {id: 3, name: "end"}
+];
 
 function App() {
 
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList);
+  
+  console.log(words);
+
   return (
     <div className='App'>
-      <StartScreen />
+      {gameStage === 'start' && <StartScreen />}
+      {gameStage === 'game' && <Game />}
+      {gameStage === 'end' && <GameOver />}
     </div>
   )
 }
