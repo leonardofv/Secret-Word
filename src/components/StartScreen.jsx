77471d12d876db4799f396b2a4125dfecae87172
Leonardo Fonteles <fonteles.leonardo@gmail.com>
2024-01-13 13:25:07 -0300

    alterei uma letra maiuscula

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -4,7 +4,7 @@ const StartScreen = ( {startGame} ) => {
   return (
     <div className='start'>
         <h1>Secret Word</h1>
-        <p>CLique no botão abaixo para começar</p>
+        <p>Clique no botão abaixo para começar</p>
         <button onClick={startGame}>Jogar</button>
     </div>
   )
